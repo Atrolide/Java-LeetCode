8a7c0b5bb7aadeb3206611869c4e2cc7b0287831
Atrolide <loszewskigrzegorz@gmail.com>
2023-07-03 10:56:55 +0200

    Ex13 Solution

--- src/main/java/org/exercises/Solutions/Ex9.java
+++ src/main/java/org/exercises/Solutions/Ex9.java
@@ -2,7 +2,7 @@ package org.exercises.Solutions;
 
 /**
  * "Palindrome Number"
- * Given an integer 'x', return 'true' if 'x' is a palindrome, and 'false' otherwise.
+ * https://leetcode.com/problems/palindrome-number/
  */
 
 public class Ex9 {
