13305ee4f416e1feaff63d668dd0509af440c32e
Atrolide <loszewskigrzegorz@gmail.com>
2023-07-07 15:56:16 +0200

    Solution for Ex20 Valid Parentheses, Added links for all tasks.

---
+++ src/main/java/org/exercises/Solutions/Ex14.java
@@ -0,0 +1,21 @@
+package org.exercises.Solutions;
+
+/**
+ * "Longest Common Prefix"
+ * https://leetcode.com/problems/longest-common-prefix/
+ */
+public class Ex14 {
+    public static String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+}
